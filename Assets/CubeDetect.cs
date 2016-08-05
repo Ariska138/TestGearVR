39262de996386dd47717c234f31c2bc2c5dbfa2a
Ariska Hidayat <Ariska Hidayat>
2016-08-05 11:12:22 +0700

    sample Gear VR

---
+++ Assets/CubeDetect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+public class CubeDetect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    bool isCubeBig = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+
+          
+                isCubeBig = !isCubeBig;
+                if (isCubeBig)
+                {
+                    transform.localScale = new Vector3(3, 3, 3);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+            
+
+        }
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            // ketika menekan tombol back
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/TextInfo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextInfo : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Mouse X") < -0.5f)
+        {
+            //Code for action on mouse moving left
+            transform.GetComponent<TextMesh>().text = ("swip left");
+        }
+        if (Input.GetAxis("Mouse X") > 0.5f)
+        {
+            //Code for action on mouse moving right
+           transform.GetComponent<TextMesh>().text = ("swip right");
+        }
+
+        if (Input.GetAxis("Mouse Y") < -0.5f)
+        {
+            //Code for action on mouse moving left
+            transform.GetComponent<TextMesh>().text = ("swip down");
+        }
+        if (Input.GetAxis("Mouse Y") > 0.5f)
+        {
+            //Code for action on mouse moving right
+            transform.GetComponent<TextMesh>().text = ("swip up");
+        }
+        
+        
+
+    }
+}
